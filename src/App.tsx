188a5dd11ab1a0a83e23a3fb28c007dbca9cb03d
Naureen Syed <naureensyed91@gmail.com>
2025-07-31 00:49:19 +0530

    file

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,47 @@
-
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
-import { ThemeProvider } from './components/Theme';
+import { ThemeProvider, useTheme } from './components/Theme'; // make sure useTheme is exported
 import './App.css';
+
+function AppContent() {
+  const { theme } = useTheme();
+
+  // Add light/dark class to <body>
+  useEffect(() => {
+    document.body.className = theme; // adds 'light' or 'dark'
+  }, [theme]);
+
+  return (
+    <div className="App">
+      <div id="site-border-left"></div>
+      <div id="site-border-right"></div>
+      <div id="site-border-top"></div>
+      <div id="site-border-bottom"></div>
+
+      <Header />
+
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
 
 function App() {
   return (
     <ThemeProvider>
       <Router>
-        <div className="App">
-          <div id="site-border-left"></div>
-          <div id="site-border-right"></div>
-          <div id="site-border-top"></div>
-          <div id="site-border-bottom"></div>
-          
-          <Header />
-          
-          <main className="main-content">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/projects" element={<Projects />} />
-              <Route path="/contact" element={<Contact />} />
-            </Routes>
-          </main>
-        </div>
+        <AppContent />
       </Router>
     </ThemeProvider>
   );
